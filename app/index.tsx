cef06b112c5db54f1da5ed1bfef41154ffbac369
seba1288 <seba128818@gmail.com>
2025-10-24 21:15:12 +0200

    added drink view

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,40 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { DrinkView } from "../components/drink-view/drink-view";
 
 export default function Index() {
+  const [isFavorite, setIsFavorite] = useState(false);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <DrinkView
+          name="Sazerac"
+          image={require("@/assets/images/drink.png")} // generowany img
+          ingredients={[
+            "Whiskey",
+            "Absynt",
+            "Gorzki angostura",
+            "Bardzo mocne",
+          ]}
+          description="Klasyczny koktajl z Nowego Orleanu. Sazerac to mocny, wyrafinowany eliksir na bazie żytniei whiskey z absyntem, nutą i aromatem cytrusów."
+          isSelected={true}
+          isFavorite={isFavorite}
+          onFavoritePress={() => setIsFavorite(!isFavorite)}
+          onPress={() => console.log("Drink card pressed")}
+        />
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1a1a1a",
+  },
+  scrollView: {
+    flex: 1,
+    paddingTop: 60,
+  },
+});
